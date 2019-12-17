e94ee41377f42f7bc2ddb1243c20b822273279d9
Aman Arora <Aman.arora@corelogic.com.au>
2019-12-17 23:29:24 +0000

    add exception pretrained path

--- FakeDetector/_nbdev.py
+++ FakeDetector/_nbdev.py
@@ -6,7 +6,8 @@ index = {"download_file": "01_data.ipynb",
          "get_files": "01_data.ipynb",
          "num_files": "01_data.ipynb",
          "url": "01_data.ipynb",
-         "get_pretrained_models": "01_data.ipynb"}
+         "get_pretrained_models": "01_data.ipynb",
+         "xceptionURL": "01_data.ipynb"}
 
 modules = ["data/core.py",
            "data/preprocess.py"]
