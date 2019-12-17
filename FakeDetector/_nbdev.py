bb440b025467ef4c56f35b398b1d846b80699c7e
Aman Arora <Aman.arora@corelogic.com.au>
2019-12-17 23:27:14 +0000

    Add pretrained url paths

--- FakeDetector/_nbdev.py
+++ FakeDetector/_nbdev.py
@@ -4,7 +4,9 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"download_file": "01_data.ipynb",
          "get_files": "01_data.ipynb",
-         "num_files": "01_data.ipynb"}
+         "num_files": "01_data.ipynb",
+         "url": "01_data.ipynb",
+         "get_pretrained_models": "01_data.ipynb"}
 
 modules = ["data/core.py",
            "data/preprocess.py"]
